2c5da7bb3a4dee42b5c05f438ae9c0bd48e475e9
SergeyKalutsky <skalutsky@gmail.com>
2022-12-01 21:41:25 +0300

    finished clean ad changed corpus source to wikipedia

--- clean.py
+++ clean.py
@@ -1,30 +1,55 @@
+import os
 import pickle
+from corus import load_wiki
+from functools import partial
 from pymystem3 import Mystem
-from itertools import chain
-from tqdm import tqdm
+from multiprocessing import Manager
+from tqdm.contrib.concurrent import process_map
 
 
-def parse_sentence(sentence):
+def get_max_count():
+    return max([int(file.split('.')[0].split('_')[-1]) for file in os.listdir('data')] + [0])
+
+
+def parse_sentence(shared_list, sentence):
     m = Mystem()
     analysises = m.analyze(sentence)
-    new_sentence = []
+    new_sentense = []
     for analysis in analysises:
         if 'analysis' in analysis:
-            gr = analysis['analysis'][0]['gr']
-            part = gr.split('=')[0].split(',')[0]
-            if part in ('A', 'S', 'V'):
-                new_sentence.append(analysis['analysis'][0]['lex'])
-    return new_sentence
+            if analysis['analysis']:
+                gr = analysis['analysis'][0]['gr']
+                part = gr.split('=')[0].split(',')[0]
+                if part in ('A', 'S', 'V'):
+                    new_sentense.append(analysis['analysis'][0]['lex'])
+    shared_list.append(new_sentense)
+    return shared_list
 
 
-with open('text.txt', 'r', encoding='utf-8') as f:
-    data = f.readlines()
+path = 'ruwiki-latest-pages-articles.xml.bz2'
+records = load_wiki(path)
 
-data = [sent.strip() for sent in chain(*[sentence.split('.') for sentence in data]) if sent not in ('', '\n')]
-sentences = []
-for sentence in tqdm(data[:]):
-    sentences.append(parse_sentence(sentence))
+if __name__ == '__main__':
+    max_count = get_max_count()
+    manager = Manager()
+    i = 0
+    while True:
+        print(i)
+        sentences = []
+        for _ in range(10):
+            record = next(records)
+            if i <= max_count:
+                continue
+            sentences += [sentence.strip()
+                          for sentence in record.text.replace('\xa0', ' ').split('.')]
+        if i <= max_count:
+            i += 1
+            continue
 
+        shared_list = manager.list()
+        r = process_map(partial(parse_sentence, shared_list),
+                        sentences, max_workers=8, chunksize=1)
+        with open(f'data/chunck_{i}.pickle', 'wb') as handle:
+            pickle.dump(list(shared_list), handle)
 
-with open('filename.pickle', 'wb') as handle:
-    pickle.dump(sentences, handle)
+        i += 1
