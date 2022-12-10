2d3364d96236f74a527a469ee6d039e53fd675f1
SergeyKalutsky <skalutsky@gmail.com>
2022-12-10 13:34:25 +0300

    works

--- frontend_vanila/script.js
+++ frontend_vanila/script.js
@@ -1,7 +1,22 @@
-let elem = document.querySelector('.input-text')
+const elem = document.querySelector('.input-text')
 
-elem.addEventListener('keypress', (event) => {
+elem.addEventListener('keypress', async (event) => {
     if (event.key === 'Enter') {
-        console.log(elem.value)
+        await fetch('http://localhost:8000/check_guess',
+            {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ word: elem.value })
+            }
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            const history = document.querySelector('.history')
+            history.innerHTML += `<p>${elem.value}   ->>>>>   ${data.rating}</p>`
+        })
     }
 })
