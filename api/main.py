de7ab8204b8843a5cfa0044f037cd9242853666b
SergeyKalutsky <skalutsky@gmail.com>
2022-12-10 13:21:54 +0300

    everything work

--- api/main.py
+++ api/main.py
@@ -1,15 +1,29 @@
 # pip install -r requirements.txt
 # python -m uvicorn main:app --reload
-from fastapi import FastAPI
 from gensim.models import KeyedVectors
 from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "*"
+]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Guess(BaseModel):
     word: str
 
 
-app = FastAPI()
 w2v = KeyedVectors.load_word2vec_format('custom_wiki_ru355texts.txt')
 secret = 'нога'
 vals = w2v.most_similar(secret, topn=100000)
@@ -23,9 +37,12 @@ async def root():
 
 @app.post("/check_guess")
 async def check_guess(guess: Guess):
+    print(guess)
     if guess.word == secret:
+        print({'rating': 1, 'error': 'ok'})
         return {'rating': 1, 'error': 'ok'}
     elif guess.word in d:
+        print({'rating': d[guess.word], 'error': 'ok'})
         return {'rating': d[guess.word], 'error': 'ok'}
+    print({"error": 'word is not found'})
     return {"error": 'word is not found'}
-
